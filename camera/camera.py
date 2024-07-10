8bf6c866f363d9a38f7445817ae5317b54de55d0
Hugo Gallardo <hgallardo@checkpt.com>
2024-07-09 19:09:08 -0600

    fix camera mode

--- camera/camera.py
+++ camera/camera.py
@@ -1,17 +1,19 @@
 from picamera2 import Picamera2
 from io import BytesIO
 from event.event_bus import EventBus
+
 class Camera:
     def __init__(self, cameraQueue) -> None:
         self.camera  = Picamera2()
+        self.camera.configure(self.camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+        self.camera.start()
         self.stream = BytesIO()
         self.cameraQueue = cameraQueue
         
 
     def stream(self):
-        self.camera .resolution = (100, 60)
-        self.camera .framerate = 30
+
         while True:
-            self.camera.capture(self.stream, format='jpeg', use_video_port=True)
-            self.cameraQueue.append(self.stream.getvalue())
+            im = self.camera.capture_array()
+            self.cameraQueue.append(im)
             EventBus.publish('stream', {'streaming': True})
--- event/notification_subscriber.py
+++
@@ -1,6 +0,0 @@
-
-class NotificationSubscriber:
-     
-     def handle_event(self, event_type, data=None):
-        if event_type == 'stream':
-            print(data)
