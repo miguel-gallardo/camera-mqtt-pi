e09c68e016a7f03e1e5ff1333fb2fa35450f944c
Hugo Gallardo <hgallardo@checkpt.com>
2024-07-09 18:57:25 -0600

    full function in main

--- controller/mqtt_controller.py
+++ controller/mqtt_controller.py
@@ -2,8 +2,9 @@
 class MQTTController:
      def __init__(self, mqttClient, cameraQueue) -> None:
          self.mqttClient = mqttClient
+         self.cameraQueue = cameraQueue
      
      def handle_event(self, event_type, data=None):
         if event_type == 'stream':
             if data:
-                self.mqttClient.publish(cameraQueue.get())
+                self.mqttClient.publish(self.cameraQueue.get())
